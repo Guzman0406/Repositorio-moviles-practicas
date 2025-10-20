3cd8a7cb780b715c0070e41f8f14ef67925f1d77
Guzman0406 <aguzmanlopez20@gmail.com>
2025-10-20 13:52:14 -0600

    avance del contador

---
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.contador
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import com.example.contador.ui.theme.ContadorTheme
+import androidx.compose.runtime.getValue // MUY importante para el 'by'
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ContadorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ContadorTheme {
+        Greeting("Android")
+    }
+}
+
+
+class counter: ViewModel(){
+    val _counter = mutableStateOf(0)
+    val counter = _counter
+    private set
+
+    fun add(){
+        _counter.value++
+    }
+}
+
+@Composable
+fun content (){
+    val counter by remember { mutableStateOf(0) }
+}
+
+@Composable
+fun botoncito(){
+
+}
